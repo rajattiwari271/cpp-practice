47599e1e1734a97dba29fd852b204e02424822a5
RAJAT <rajattiwari7611@gmail.com>
2026-09-26 12:18:28 +0530

    Insert at head in Doubly LL

---
+++ 012.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public : 
+    int data ;
+    Node* prev;
+    Node* next;
+     Node() {
+        this->data =0;
+        this->prev = NULL;
+        this->next=NULL;
+     }
+     Node (int data) {
+        this->data = data;
+        this->prev=NULL;
+        this->next=NULL;
+     }
+
+
+};
+
+void print(Node* head){
+    Node* temp = head ;
+    while(temp !=NULL){
+        cout<<temp->data<<" "<<endl;
+        temp = temp ->next;
+    }
+}
+
+int getLength(Node* head){
+    int len = 0;
+    Node* temp = head ;
+    while(temp != NULL) {
+        temp = temp->next;
+        len ++;
+
+    }
+    return len ;
+}
+
+void insertAtHead(Node* &head ,Node* &tail , int data){
+    if(head==NULL){
+        Node* newNode = new Node(data);
+        head = newNode;
+        tail = newNode;
+        
+    }
+    else {
+        Node* newNode = new Node(data);
+        newNode->next = head;
+        head ->prev = newNode;
+        head = newNode;
+    }
+}
+
+int main (){
+
+Node* first = new Node(10);
+Node* second = new Node(20);
+Node* third = new Node(30);
+Node* head = first;
+Node* tail = third;
+
+first->next = second;
+second->prev = first;
+second->next = third;
+third->prev=second;
+
+print (first);
+cout<<endl;
+
+insertAtHead(head,tail,101);
+cout<<endl;
+
+print(head);
+
+
+
+    return 0;
+
+
+}
